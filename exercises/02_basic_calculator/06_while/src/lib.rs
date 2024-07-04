0f95405e1076de638cb7ad2fb3376a378df5ed5a
realwanwanyi <realwanwanyi@gmail.com>
2024-07-04 17:15:05 +0800

    fix 2.6, 2.7 loops

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,15 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut i: u32 = n;
+    let mut fac: u32 = 1;
+
+    while i > 0 {
+        fac *= i;
+        i -= 1;
+    }
+
+    fac
 }
 
 #[cfg(test)]
